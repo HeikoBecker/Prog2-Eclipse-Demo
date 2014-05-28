4208ccf8a546e384c028d7cdb43d655cf3cd80ed
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-05-28 11:49:20 +0200

    implemented core structure of the project

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+class Main
+{
+  public static void main( String args[] )
+  {
+    final int array[] = {5, 0, -18, 13, 42, 23, 137, -4};
+
+    /* TODO
+     *
+     * - sortierte verkettete Liste entwerfen
+     * - Elemente aus 'array' in die Liste einfügen
+     * - die Liste in aufsteigender Ordnung ausgeben
+     *
+     */
+
+    return;
+  }
+};
