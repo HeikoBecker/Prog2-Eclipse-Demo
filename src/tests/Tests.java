571d2603a0f9cdcb4d54f93212203a8f5c3badd1
Heiko Becker <heiko.becker@rocketmail.com>
2014-05-29 12:34:49 +0200

    Added two tests

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -9,11 +9,28 @@ public class Tests {
 	
 	@Test
 	public void testInsertionAndHead() {
-		SortedLinkedList list = new SortedLinkedList(3);
+		SortedLinkedList list = new SortedLinkedList(0);
 		assertTrue(list.getNext() == null);
-		SortedLinkedList backup = list;
+		list.insert(3);
 		list.insert(-1);
-		assertTrue(list.getNext() == backup);
+		list = list.getNext();
+		assertTrue(list.getData() == -1);
+		list = list.getNext();
+		assertTrue(list.getData() == 3);
 	}
-
+	
+	@Test
+	public void testNullAsEnd(){
+		SortedLinkedList list = new SortedLinkedList(-1);
+		
+		for(int i = 0; i < 10; i++){
+			list.insert(i);
+		}
+		
+		for(int i = 0; i < 11; i++){
+			assertTrue(list.getData() == i-1);
+			list = list.getNext();
+		}
+		assertTrue(list == null);
+	}
 }
